8620dcb20b3f5e1dc2c8decc40a07860185ca19d
Atul Ranjan jha <99534159+AtulRanjanJha@users.noreply.github.com>
2025-04-14 13:10:47 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import AnimePage from "./pages/AnimePage";
-import MangaPage from "./pages/MangaPage";
 
 const App: React.FC = () => {
+  console.log("App is rendering");
+
   return (
-    <Router>
-      <nav>
-        <Link to="/anime">Anime</Link> | <Link to="/manga">Manga</Link>
-      </nav>
-      <Routes>
-        <Route path="/anime/*" element={<AnimePage />} />
-        <Route path="/manga/*" element={<MangaPage />} />
-      </Routes>
-    </Router>
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
   );
 };
 
